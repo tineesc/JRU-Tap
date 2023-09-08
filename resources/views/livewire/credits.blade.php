858c56caf9bd6260ad151baef4826197a9832ed8
XirTech <john.darlucio022@gmail.com>
2023-09-09 01:55:01 +0800

    credits responsive

--- resources/views/livewire/credits.blade.php
+++ resources/views/livewire/credits.blade.php
@@ -1,49 +1,33 @@
 <div class="py-12">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="bg-transparent dark:bg-gray-800 overflow-hidden shadow-md sm:rounded-lg">
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 rounded-lg shadow-md mb-3">
-                <div class="bg-sky-600 p-5">
-                    <h2 class="font-semibold text-2xl">Virtual Card</h2>
-                    <h2 class="font-semibold text-2xl">Modern Jeepney (ACTONA)</h2>
-                    <div class="pt-16 pb-10 flex flex-col md:flex-row justify-between items-center">
-                        <img src="/image/chip.png" alt="" class="max-w-full h-10 md:w-32 md:h-16 mb-4 md:mb-0" />
-                        <h2 class="font-semibold text-2xl">{{ Auth::user()->name }}</h2>
-                    </div>
-                    <div class="flex justify-between">
-                        <p class="text-3xl pt-6 font-bold">
-                            @if (Auth::user()->card_id)
-                                {{ Auth::user()->card_id }}
-                            @else
-                                No Register Card ID
-                            @endif
-                        </p>
-                        <p class="text-3xl pt-6">Logo</p>
-                    </div>
-                </div>
-            </div>
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 rounded shadow-md">
-                <div class="grid place-content-center bg-purple-50">
+
+            <!-- Main Grid -->
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 rounded shadow-md mb-3">
+                <!-- Top-Up Credits Form -->
+                <div class="bg-purple-50">
+                    <!-- Content -->
                     <form action="{{ route('process') }}">
-                        <h2 class="font-semibold text-3xl text-center text-slate-600 py-5">Top up Credits</h2>
-                        <div class="mx-5 py-2">
+                        <h2 class="font-semibold text-3xl text-center text-slate-600 py-5 mx-auto">Top up Credits</h2>
+                        <div class="mx-6 py-2">
                             <x-label for="credits" value="{{ __('Credits') }}" />
-                            <x-input id="credits" class="block mt-1 w-full md:w-96" type="text" name="credits"
-                                :value="old('credits')" required autofocus autocomplete="credits"
-                                placeholder="amount" required />
+                            <x-input id="credits" class="block mt-1 w-full md:w-92 mx-auto" type="text" name="credits"
+                                :value="old('credits')" required autofocus autocomplete="credits" placeholder="amount"
+                                required />
                         </div>
-                        <div class="mx-5 py-2">
+                        <div class="mx-6 py-2">
                             <x-label for="cardID" value="{{ __('Card ID Number') }}" />
-                            <x-input id="cardID" class="block mt-1 w-full md:w-96" type="text" name="cardID"
-                                :value="old('cardID')" required autofocus autocomplete="cardID"
-                                placeholder="Card ID Number" required />
+                            <x-input id="cardID" class="block mt-1 w-full md:w-92 mx-auto" type="text" name="cardID"
+                                :value="old('cardID')" required autofocus autocomplete="cardID" placeholder="Card ID Number"
+                                required />
                         </div>
 
-                        <div class="mx-5 py-2">
+                        <div class="mx-6 py-2">
                             <label for="payment"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an
                                 option</label>
                             <select id="payment" name="payment"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 md:w-96 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                class="w-full  md:w-70 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                 <option selected>Choose a country</option>
                                 <option value="pay">Card</option>
                                 <option value="link">Link</option>
@@ -55,8 +39,11 @@
                         </div>
                     </form>
                 </div>
-                <div class="grid place-content-center bg-purple-50 text-center rounded shadow-md">
+
+                <!-- Balance Credits -->
+                <div class="grid place-content-center bg-purple-50 text-center">
                     <form action="">
+                        <!-- Content -->
                         <h2 class="font-semibold text-2xl  text-slate-600 py-2">Balance Credits</h2>
                         <p class="font-bold text-xl">
                             @if (Auth::user()->card_amount)
@@ -70,25 +57,50 @@
                         </div>
                     </form>
                 </div>
+
+                <!-- Driver Wallet Balance (Only for Role 4) -->
                 @role(4)
-                <div class="grid place-content-center bg-purple-50 text-center rounded shadow-md">
-                    <form action="">
-                        <h2 class="font-semibold text-2xl  text-slate-600 py-2">Driver Wallet Balance</h2>
-                        <p class="font-bold text-xl">
-                            @if (Auth::user()->wallet_amount)
-                                {{ Auth::user()->wallet_amount }}
-                            @else
-                                No Balance to show
-                            @endif
-                        </p>
-                        <div class="py-5 mx-5 text-center">
-                            <x-button>Send</x-button>
-                            <x-button>Withdraw</x-button>
-                        </div>
-                    </form>
-                </div>
+                    <div class="grid place-content-center bg-purple-50 text-center">
+                        <form action="">
+                            <!-- Content -->
+                            <h2 class="font-semibold text-2xl  text-slate-600 py-2">Driver Wallet Balance</h2>
+                            <p class="font-bold text-xl">
+                                @if (Auth::user()->wallet_amount)
+                                    {{ Auth::user()->wallet_amount }}
+                                @else
+                                    No Balance to show
+                                @endif
+                            </p>
+                            <div class="py-5 mx-5 text-center">
+                                <x-button>Send</x-button>
+                                <x-button>Withdraw</x-button>
+                            </div>
+                        </form>
+                    </div>
                 @endrole
             </div>
+
+            <!-- Virtual Card Section -->
+            <div class="bg-sky-600 p-5">
+                <!-- Content -->
+                <h2 class="font-semibold text-2xl">Virtual Card</h2>
+                <h2 class="font-semibold text-2xl">Modern Jeepney (ACTONA)</h2>
+                <div class="pt-16 pb-10 flex flex-col md:flex-row justify-between items-center">
+                    <img src="/image/chip.png" alt="" class="max-w-full h-10 md:w-32 md:h-16 mb-4 md:mb-0" />
+                    <h2 class="font-semibold text-2xl">{{ Auth::user()->name }}</h2>
+                </div>
+                <div class="flex justify-between">
+                    <p class="text-3xl pt-6 font-bold">
+                        @if (Auth::user()->card_id)
+                            {{ Auth::user()->card_id }}
+                        @else
+                            No Register Card ID
+                        @endif
+                    </p>
+                    <p class="text-3xl pt-6">Logo</p>
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
