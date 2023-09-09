71771c04a9cc24427f67e1a05b996a05d44088c3
XirTech <john.darlucio022@gmail.com>
2023-09-09 14:57:12 +0800

    password validation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,22 +13,29 @@
 
                 <div class="mb-4">
                     <x-label for="name" value="{{ __('Name') }}" />
-                    <x-input id="name" class="block w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                    <x-input id="name" class="block w-full" type="text" name="name" :value="old('name')" required
+                        autofocus autocomplete="name" />
                 </div>
 
                 <div class="mb-4">
                     <x-label for="email" value="{{ __('Email') }}" />
-                    <x-input id="email" class="block w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                    <x-input id="email" class="block w-full" type="email" name="email" :value="old('email')"
+                        required autocomplete="username" />
                 </div>
 
                 <div class="mb-4">
                     <x-label for="password" value="{{ __('Password') }}" />
-                    <x-input id="password" class="block w-full" type="password" name="password" required autocomplete="new-password" />
+                    <x-input id="password" class="block w-full" type="password" name="password" required
+                        autocomplete="new-password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                        title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" />
                 </div>
 
                 <div class="mb-4">
                     <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                    <x-input id="password_confirmation" class="block w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                    <x-input id="password_confirmation" class="block w-full" type="password"
+                        name="password_confirmation" required autocomplete="new-password"
+                        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                        title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" />
                 </div>
 
                 @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
@@ -38,8 +45,18 @@
                                 <x-checkbox name="terms" id="terms" required />
                                 <div class="ml-2">
                                     {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Privacy Policy').'</a>',
+                                        'terms_of_service' =>
+                                            '<a target="_blank" href="' .
+                                            route('terms.show') .
+                                            '" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">' .
+                                            __('Terms of Service') .
+                                            '</a>',
+                                        'privacy_policy' =>
+                                            '<a target="_blank" href="' .
+                                            route('policy.show') .
+                                            '" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">' .
+                                            __('Privacy Policy') .
+                                            '</a>',
                                     ]) !!}
                                 </div>
                             </div>
@@ -48,7 +65,9 @@
                 @endif
 
                 <div class="flex items-center justify-end mb-4">
-                    <a wire:navigate class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                    <a wire:navigate
+                        class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        href="{{ route('login') }}">
                         {{ __('Already registered?') }}
                     </a>
 
@@ -60,7 +79,8 @@
 
             <!-- Image -->
             <div class="sm:w-52 md:w-1/2 mx-auto mb-8">
-                <img class="w-full" src="{{ url('http://clipartmag.com/images/jeep-clipart-40.jpg') }}" alt="Image" />
+                <img class="w-full" src="{{ url('http://clipartmag.com/images/jeep-clipart-40.jpg') }}"
+                    alt="Image" />
             </div>
         </div>
     </x-authentication-card>
