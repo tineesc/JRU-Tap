ee4396f9c7d892725e7662d5e0e8ebd1b6ff63c2
XirTech <john.darlucio022@gmail.com>
2023-09-09 20:13:57 +0800

    add new table

---
+++ database/migrations/2023_09_09_111338_create_revenue_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('revenue', function (Blueprint $table) {
+            $table->id();
+            $table->string('wallet_id')->nullable();
+            $table->string('wallet_amount')->nullable();
+            $table->integer('fare');
+            $table->string('jnumber');
+            $table->string('payment_method');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('revenue');
+    }
+};
