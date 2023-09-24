2a0efc12fd814fb07d6223396338bb68fbd31ae4
XirTech <john.darlucio022@gmail.com>
2023-09-24 19:34:55 +0800

    fix table

--- app/Livewire/JeepRevenueTable.php
+++ app/Livewire/JeepRevenueTable.php
@@ -2,12 +2,19 @@
 
 namespace App\Livewire;
 
+use App\Models\Revenue;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class JeepRevenueTable extends Component
 {
+    use WithPagination;
+    
     public function render()
     {
-        return view('livewire.jeep-revenue-table');
+        $items = Revenue::orderBy('id','DESC')->get();
+        return view('livewire.jeep-revenue-table', [
+            'items' => Revenue::paginate(10),
+        ],compact('items'));
     }
 }
