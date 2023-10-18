20820f1b31d4a590d7be06f14549e6e6d069fc9e
XirTech <john.darlucio022@gmail.com>
2023-10-18 16:49:54 +0800

    change design in credits

--- resources/views/livewire/credits.blade.php
+++ resources/views/livewire/credits.blade.php
@@ -2,78 +2,76 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="bg-transparent dark:bg-gray-800 overflow-hidden shadow-md sm:rounded-lg">
 
-            <!-- Main Grid -->
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 rounded shadow-md mb-3">
-                <!-- Top-Up Credits Form -->
-                <div class="bg-purple-50">
-                    <!-- Content -->
-                    <form action="{{ route('pay') }}">
-                        @csrf
-                        <h2 class="font-semibold text-3xl text-center text-slate-600 py-5 mx-auto">Top up Credits</h2>
-                        <div class="mx-6 py-2">
-                            <x-label for="credits" value="{{ __('Credits') }}" />
-                            <x-input wire:model="credits" id="credits" class="block mt-1 w-full md:w-92 mx-auto"
-                                type="text" name="credits" required autofocus autocomplete="credits"
-                                placeholder="amount" required {{-- Numbers only Validation --}}
-                                oninput="this.value = this.value.replace(/[^0-9.]/g, ''); this.value = this.value.replace(/(\..*)\./g, '$1');" />
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 rounded shadow-md mb-3">
+               <!-- Top-Up Credits Form -->
+               <div class="bg-purple-50">
+                <!-- Content -->
+                <form action="{{ route('pay') }}">
+                    @csrf
+                    <h2 class="font-semibold text-3xl text-center text-slate-600 py-5 mx-auto">Top up Credits</h2>
+                    <div class="mx-6 py-2">
+                        <x-label for="credits" value="{{ __('Credits') }}" />
+                        <x-input wire:model="credits" id="credits" class="block mt-1 w-full md:w-92 mx-auto"
+                            type="text" name="credits" required autofocus autocomplete="credits"
+                            placeholder="amount" required {{-- Numbers only Validation --}}
+                            oninput="this.value = this.value.replace(/[^0-9.]/g, ''); this.value = this.value.replace(/(\..*)\./g, '$1');" />
 
-                        </div>
-                        <div class="mx-6 py-2">
-                            <x-label for="cardid" value="{{ __('Card ID Number') }}" />
-                            <x-input wire:model="cardid" id="cardid" class="block mt-1 w-full md:w-92 mx-auto"
-                                type="text" name="cardid" required autofocus autocomplete="cardid"
-                                placeholder="Card ID Number" required {{-- Numbers only Validation --}}
-                                oninput="this.value = this.value.replace(/[^0-9.]/g, ''); this.value = this.value.replace(/(\..*)\./g, '$1');" />
-                        </div>
-
-                        <div class="py-5 mx-5 text-center">
-                            <x-button class="uppercase">add credits</x-button>
-                        </div>
-                    </form>
-                </div>
-
-                <!-- Balance Credits -->
-                <div class="grid place-content-center bg-purple-50 text-center">
-                    <form action="">
-                        <!-- Content -->
-                        <h2 class="font-semibold text-2xl  text-slate-600 py-2">Card Balance Credits</h2>
-                        <p class="font-bold text-xl">
-                            @if ($cardBalance !== null)
-                                <p class="semi-bold">{{ $cardBalance }}</p>
-                            @else
-                                <p>No Balance to show</p>
-                            @endif
-                        </p>
-
-                        <div class="py-5 mx-5 text-center">
-                            <x-button>Register Card</x-button>
-                        </div>
-                    </form>
-                </div>
-
-                <!-- Driver Wallet Balance (Only for Role 4) -->
-                @role(4)
-                    <div class="grid place-content-center bg-purple-50 text-center">
-                        <form>
-                            <!-- Content -->
-                            <h2 class="font-semibold text-2xl  text-slate-600 py-2">Driver Wallet Balance</h2>
-                            <p class="font-bold text-xl">
-                                @if (Auth::user()->wallet_balance)
-                                    {{ Auth::user()->wallet_balance }}
-                                @else
-                                    No Balance to show
-                                @endif
-                            </p>
-                            <div class="py-5 mx-5 text-center">
-                                <x-button>Send</x-button>
-                                <x-button>Withdraw</x-button>
-                            </div>
-                        </form>
                     </div>
-                @endrole
-            </div>
+                    <div class="mx-6 py-2">
+                        <x-label for="cardid" value="{{ __('Card ID Number') }}" />
+                        <x-input wire:model="cardid" id="cardid" class="block mt-1 w-full md:w-92 mx-auto"
+                            type="text" name="cardid" required autofocus autocomplete="cardid"
+                            placeholder="Card ID Number" required {{-- Numbers only Validation --}}
+                            oninput="this.value = this.value.replace(/[^0-9.]/g, ''); this.value = this.value.replace(/(\..*)\./g, '$1');" />
+                    </div>
 
-            <!-- Virtual Card Section -->
+                    <div class="py-5 mx-5 text-center">
+                        <x-button class="uppercase">add credits</x-button>
+                    </div>
+                </form>
+            </div>
+            
+              <!-- Balance Credits -->
+              <div class="grid place-content-center bg-purple-50 text-center">
+                <form action="">
+                    <!-- Content -->
+                    <h2 class="font-semibold text-2xl  text-slate-600 py-2">Card Balance Credits</h2>
+                    <p class="font-bold text-xl">
+                        @if ($cardBalance !== null)
+                            <p class="semi-bold">{{ $cardBalance }}</p>
+                        @else
+                            <p>No Balance to show</p>
+                        @endif
+                    </p>
+
+                    <div class="py-5 mx-5 text-center">
+                        <x-button>Register Card</x-button>
+                    </div>
+                </form>
+            </div>
+            
+               <!-- Driver Wallet Balance (Only for Role 4) -->
+               @role(4)
+               <div class="grid place-content-center bg-purple-50 text-center">
+                   <form>
+                       <!-- Content -->
+                       <h2 class="font-semibold text-2xl  text-slate-600 py-2">Driver Wallet Balance</h2>
+                       <p class="font-bold text-xl">
+                           @if (Auth::user()->wallet_balance)
+                               {{ Auth::user()->wallet_balance }}
+                           @else
+                               No Balance to show
+                           @endif
+                       </p>
+                       <div class="py-5 mx-5 text-center">
+                           <x-button>Send</x-button>
+                           <x-button>Withdraw</x-button>
+                       </div>
+                   </form>
+               </div>
+           @endrole
+            
+               <!-- Virtual Card Section -->
             <div class="bg-sky-600 p-5">
                 <!-- Content -->
                 <h2 class="font-semibold text-2xl">Virtual Card</h2>
@@ -94,6 +92,8 @@
                         onclick="copyContent()">Copy to Clipboard</button>
                 </div>
             </div>
+            </div>
+            
 
             <div class="hidden sm:max-w:hidden md:block lg:block xl:block">
                 <!-- Content for Laptop and Desktop View -->
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -12,21 +12,21 @@
                         <label for="location"
                             class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Location</label>
                         <select wire:model="locationFilter" id="location"
-                            class="block w-full p-4 text-sm text-white border border-gray-300 rounded-lg bg-transparent focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            <option  class="text-slate-950" value="">All Locations</option>
+                            class="block w-full p-4 text-sm text-slate-950 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option  class="text-slate-950" value="">Locations</option>
                             @foreach ($locations as $loc)
                                 <option class="text-lg text-slate-950" value="{{ $loc->location }}">• {{ $loc->location }}</option>
                             @endforeach
                         </select>
-                    </div>
+                    </div>  
 
                     {{-- Destination Button --}}
                     <div class="flex flex-1 mx-2">
                         <label for="destination"
                             class="mt-4 mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Destination</label>
                         <select wire:model="destinationFilter" id="destination"
-                            class="block w-full p-4 text-sm text-white border border-gray-300 rounded-lg bg-transparent focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            <option class="text-slate-950">All Destinations</option>
+                            class="block w-full p-4 text-sm text-slate-950 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option class="text-slate-950">Destinations</option>
                             @foreach ($destinations as $dest)
                                 <option class="text-lg text-slate-950" value="{{ $dest->destination }}">• {{ $dest->destination }}</option>
                             @endforeach
@@ -36,10 +36,10 @@
 
                     {{-- Filter Search Button --}}
                     <button type="submit"
-                        class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Search
+                        class="p-2.5 ml-2 text-sm font-medium text-white bg-transparent rounded-lg border border-stone-100 hover:bg-stone-400 focus:ring-w-4 focus:outline-none focus:ring-stone-100 dark:bg-stone-400 dark:hover:bg-stone-500 dark:focus:ring-stone-600">
+                    
 
-                        <span class="sr-only">Search</span>
+                        <span class="capitalize font-semibold text-lg">Search</span>
                     </button>
 
                     {{-- Reload Page Button  --}}
