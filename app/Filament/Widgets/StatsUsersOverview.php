2690b8e12febca578503059f85f4e2cfac452d66
XirTech <john.darlucio022@gmail.com>
2023-10-21 10:47:21 +0800

    Fix chart

---
+++ app/Filament/Widgets/StatsUsersOverview.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget\Card;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+
+class StatsUsersOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Card::make('Total Revenue Payments', '192.1k')
+                ->description('32k increase')
+                ->descriptionIcon('heroicon-s-arrow-trending-up')
+                ->color('success'),
+            Card::make('Total Credits Payments', '21%')
+                ->description('7% increase')
+                ->descriptionIcon('heroicon-s-arrow-trending-down')
+                ->color('danger'),
+            Card::make('Average Total Credits Payment', '3:12')
+                ->description('3% increase')
+                ->descriptionIcon('heroicon-s-arrow-trending-up')
+                ->color('success'),
+        ];
+    }
+}
