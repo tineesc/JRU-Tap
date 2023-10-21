30e9198d0347c42ca4a72eacf117e33607180fd8
XirTech <john.darlucio022@gmail.com>
2023-10-21 22:54:56 +0800

    add query for jeeps

--- app/Filament/Widgets/JeepStatsOverview.php
+++ app/Filament/Widgets/JeepStatsOverview.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Filament\Widgets\StatsOverviewWidget\Card;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
@@ -10,10 +12,15 @@ class JeepStatsOverview extends BaseWidget
 {
     protected function getStats(): array
     {
+        $jeep = DB::select('SELECT COUNT(*) as count FROM jeeps')[0]->count;
+        $queue = DB::select('SELECT COUNT(*) as count FROM queues')[0]->count;
+        $usersWithDriverRole = User::whereHas('roles', function ($query) {
+            $query->where('name', 'driver');
+        })->count();
         return [
-            Card::make('Total Jeeps', '35'),
-            Card::make('Total Driver', '20'),
-            Card::make('Total Queues', '120'),
+            Card::make('Total Jeeps', $jeep),
+            Card::make('Total Driver', $usersWithDriverRole),
+            Card::make('Total Queues', $queue),
         ];
     }
 }
