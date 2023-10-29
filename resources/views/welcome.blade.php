f267686967c8d7b3013eab52079ca1fdd3806ca8
XirTech <john.darlucio022@gmail.com>
2023-10-30 01:04:06 +0800

    route to credits the landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,8 +37,8 @@
                 <div class="flex items-center lg:order-2">
                     @if (Route::has('login'))
                         @auth
-                            <a href="{{ url('/dashboard') }}"
-                                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                            <a href="{{ url('/credits') }}"
+                                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
                         @else
                             @if (Route::has('register'))
                                 <a href="{{ route('register') }}"
