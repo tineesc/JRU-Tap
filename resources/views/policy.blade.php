00fe014ff76b141ce39c8d05f80a3e6f7090b26a
XirTech <john.darlucio022@gmail.com>
2023-10-30 02:06:05 +0800

    terms and policy

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -6,7 +6,44 @@
             </div>
 
             <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg prose dark:prose-invert">
-                {!! $policy !!}
+                {{-- {!! $policy !!} --}}
+
+                <div class="text-center">
+                    <h3 class="uppercase"><strong>Privacy Policy of Terminal Jeep Company</strong></h3>
+                    <ul>
+                        <li class="text-left">
+                           <h3> <strong>Information Collection</strong></h3>
+                           <p class="text-center"> We collect personal information, including but not limited to name, contact details, and payment information, to provide and improve our services. We may also collect non-personal information for analytical purposes.</p>
+                        </li>
+
+                        <li class="text-left">
+                            <h3> <strong>Data Usage</strong></h3>
+                           <p class="text-center"> We use the information collected to facilitate bookings, process payments, communicate with users, and enhance user experience. We do not sell or disclose personal information to third parties without consent, except as required by law.</p>
+                        </li>
+
+                        <li class="text-left">
+                            <h3> <strong>Security</strong></h3>
+                           <p class="text-center"> We implement reasonable security measures to protect personal information from unauthorized access, disclosure, alteration, and destruction. However, no data transmission over the internet or stored on a server can be guaranteed to be 100% secure. </p>
+                        </li>
+
+                        <li class="text-left">
+                            <h3> <strong>Cookies</strong></h3>
+                           <p class="text-center"> We use cookies to enhance user experience and gather information about usage patterns. Users can control the use of cookies at the individual browser level. </p>
+                        </li>
+
+                        <li class="text-left">
+                            <h3> <strong>Changes to Privacy Policy</strong></h3>
+                           <p class="text-center">Terminal Jeep Company reserves the right to update this privacy policy at any time. Users will be notified of any changes, and continued use of our services after the changes constitute acceptance of the revised policy.
+
+                            These Terms and Conditions and Privacy Policy constitute the entire agreement between you and Terminal Jeep Company. If any provision is deemed invalid, the remaining provisions will continue in full force and effect.
+                            
+                            Effective Date: [10/30/2023]
+                            
+                            For any inquiries or concerns regarding these terms, please contact us at [help@actona.com]. </p>
+                        </li>
+                    </ul>
+                    <a href="/register" class="uppercase text-md bg-yellow-300 p-3 rounded-lg">Back</a>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -6,7 +6,39 @@
             </div>
 
             <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg prose dark:prose-invert">
-                {!! $terms !!}
+                {{-- {!! $terms !!} --}}
+
+                <div class="text-center">
+                    <h3 class="uppercase"><strong>Terms and Conditions of Terminal Jeep Company</strong></h3>
+                    <ul>
+                        <li class="text-left">
+                           <h3> <strong>Acceptance of Terms</strong></h3>
+                           <p class="text-center"> By accessing or using the services provided by Terminal Jeep Company, you agree to comply with and be bound by these terms and conditions. If you do not agree with any part of these terms, you may not use our services.</p>
+                        </li>
+
+                        <li class="text-left">
+                            <h3> <strong>Services</strong></h3>
+                           <p class="text-center"> Terminal Jeep Company provides a platform for users to explore, select, and book Jeep services. We reserve the right to modify, suspend, or discontinue any aspect of our services at any time without notice.</p>
+                        </li>
+
+                        <li class="text-left">
+                            <h3> <strong>User Responsibilities</strong></h3>
+                           <p class="text-center"> You agree to provide accurate and complete information when using our services. You are responsible for maintaining the confidentiality of your account information and for all activities that occur under your account. </p>
+                        </li>
+
+                        <li class="text-left">
+                            <h3> <strong>Booking and Payments</strong></h3>
+                           <p class="text-center">  Booking of Jeep services is subject to availability. Payments for services must be made in accordance with our payment policies. Cancellations and refunds are subject to our cancellation policy, as outlined on our platform. </p>
+                        </li>
+
+                        <li class="text-left">
+                            <h3> <strong>Intellectual Property</strong></h3>
+                           <p class="text-center"> All content and materials available on the Terminal Jeep Company platform, including but not limited to logos, text, graphics, images, and software, are the property of Terminal Jeep Company and are protected by intellectual property laws. </p>
+                        </li>
+                    </ul>
+                    <a href="/register" class="uppercase text-md bg-yellow-300 p-3 rounded-lg">Back</a>
+                </div>
+
             </div>
         </div>
     </div>
