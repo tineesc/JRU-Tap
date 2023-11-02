0cddd672c539b9ca87266409188a31e9e168416d
XirTech <john.darlucio022@gmail.com>
2023-11-02 16:35:49 +0800

    fix permission database

--- app/Filament/Resources/PermissionResource/Pages/EditPermission.php
+++ app/Filament/Resources/PermissionResource/Pages/EditPermission.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\PermissionResource\Pages;
 
-use App\Filament\Resources\PermissionResource;
+use App\Models\User;
 use Filament\Actions;
+use Illuminate\Support\Facades\Auth;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Notifications\Actions\Action;
+use App\Filament\Resources\PermissionResource;
 
 class EditPermission extends EditRecord
 {
@@ -16,5 +20,35 @@ class EditPermission extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        $permission = $this->record;
+
+        $auth = Auth::user();
+        // Get a list of users with roles "admin" and "moderator"
+        $usersWithRoles = User::whereHas('roles', function ($query) {
+            $query->whereIn('id', [1, 2]);
+        })->get();
+
+        // Send the notification to each user
+        foreach ($usersWithRoles as $user) {
+            $notification = Notification::make()
+                ->warning()
+                ->icon('heroicon-o-key')
+                ->title('Permission Resource Modified')
+                ->body(" Permission {$permission->name} Modified by {$auth->name}!")
+                ->actions([Action::make('View')->url(PermissionResource::getUrl('edit', ['record' => $permission]))])
+                ->sendToDatabase($user);
+        }
+
+        return $notification; // or return a notification if needed
+    }
+   
    
 }
