08f57af8bb07064374013284ef620ba46359bf66
XirTech <john.darlucio022@gmail.com>
2023-11-02 16:14:44 +0800

    fix driver notification

--- app/Filament/Resources/JeepResource/Pages/EditJeep.php
+++ app/Filament/Resources/JeepResource/Pages/EditJeep.php
@@ -33,38 +33,56 @@ class EditJeep extends EditRecord
     {
         $jeep = $this->record;
         $auth = Auth::user();
-
+    
         $driverName = $jeep->driver; // Assuming $jeep->driver contains the driver's name
-
+    
         // Find the user by name and retrieve their ID
         $driver = User::where('name', $driverName)->value('id');
-
+        $driverUser = User::find($driver);
+    
         $notification = null; // Initialize the notification variable
-
-        // Attempt to send the notification to the last selected driver
-        if (is_numeric($driver)) {
-            $driverUser = User::find($driver);
-                if ($driverUser instanceof \Illuminate\Contracts\Auth\Authenticatable) {
-                    $notification = Notification::make()
-                        ->warning()
-                        ->icon('heroicon-o-truck')
-                        ->title('Jeep Resource Modified')
-                        ->body("Jeep {$jeep->jnumber} Assign to Driver {$jeep->driver}  !")
-                        ->actions([Action::make('View')->url(JeepResource::getUrl('edit', ['record' => $jeep]))])
-                        ->sendToDatabase([$driverUser, $auth]);
-                    }
-        }
-
-        // If the notification to the last driver was not sent, send it to the authenticated user
-        if (!$notification && $auth instanceof \Illuminate\Contracts\Auth\Authenticatable) {
+    
+        if ($jeep->driver === null) {
             $notification = Notification::make()
                 ->info()
                 ->icon('heroicon-o-truck')
                 ->title('Jeep Resource Modified')
-                ->body('Walang Driver')
+                ->body('No Driver Assigned!')
+                ->sendToDatabase($auth);
+        } elseif ($jeep->wasChanged('begin')) {
+            $notification = Notification::make()
+                ->success()
+                ->icon('heroicon-o-truck')
+                ->title('Jeep Resource Modified')
+                ->body("Time IN!")
+                ->actions([Action::make('View')->url(JeepResource::getUrl('edit', ['record' => $jeep]))])
+                ->sendToDatabase([$driverUser, $auth]);
+        } elseif ($jeep->wasChanged('queue')) {
+            $notification = Notification::make()
+                ->success()
+                ->icon('heroicon-o-truck')
+                ->title('Jeep Resource Modified')
+                ->body("Queue!")
+                ->actions([Action::make('View')->url(JeepResource::getUrl('edit', ['record' => $jeep]))])
+                ->sendToDatabase([$driverUser, $auth]);
+        } elseif ($jeep->wasChanged('end')) {
+            $notification = Notification::make()
+                ->success()
+                ->icon('heroicon-o-truck')
+                ->title('Jeep Resource Modified')
+                ->body("Time OUT!")
+                ->actions([Action::make('View')->url(JeepResource::getUrl('edit', ['record' => $jeep]))])
+                ->sendToDatabase([$driverUser, $auth]);
+        } elseif ($jeep->driver !== null) {
+            $notification = Notification::make()
+                ->success()
+                ->icon('heroicon-o-truck')
+                ->title('Jeep Resource Modified')
+                ->body("Driver {$jeep->driver} Assigned to {$jeep->jnumber}")
                 ->sendToDatabase($auth);
         }
-
-        return $notification; // Return the notification or null if no valid user was found
+    
+        return $notification;
     }
+    
 }
