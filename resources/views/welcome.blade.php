3c2cd9bad44a03e22c2ea03765cf1dfaa054fa0a
XirTech <john.darlucio022@gmail.com>
2023-11-11 12:08:41 +0800

    update landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,11 +51,11 @@
                 </div>
                 <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                     <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                        <li>
+                        {{-- <li>
                             <a href="#home"
                                 class="block py-2 pr-4 pl-3 text-white rounded bg-blue-700 lg:bg-transparent lg:text-gray-700 lg:p-0 dark:text-white"
                                 aria-current="page">Dashboard</a>
-                        </li>
+                        </li> --}}
                         <li>
                             <a href="#services"
                                 class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Services</a>
